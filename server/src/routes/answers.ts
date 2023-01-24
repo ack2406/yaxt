2c45f3b5a258c1e80d1db66b952100bad1f20f38
ack2406 <ack9222@gmail.com>
2023-01-24 22:33:43 +0100

    fix authentication in answers and questions routes

--- server/src/routes/answers.ts
+++ server/src/routes/answers.ts
@@ -6,14 +6,15 @@ import {
   updateAnswer,
   deleteAnswer,
 } from "../controllers/answers";
+import { checkAuth } from "../config/utils";
 
 const AnswerRouter = express.Router();
 
-AnswerRouter.route("/").get(getAllAnswers).post(createAnswer);
+AnswerRouter.route("/").get(getAllAnswers).post(checkAuth, createAnswer);
 
 AnswerRouter.route("/:id")
   .get(getAnswerById)
-  .put(updateAnswer)
-  .delete(deleteAnswer);
+  .put(checkAuth, updateAnswer)
+  .delete(checkAuth, deleteAnswer);
 
 export { AnswerRouter };
--- server/src/routes/questions.ts
+++ server/src/routes/questions.ts
@@ -6,14 +6,15 @@ import {
   updateQuestion,
   deleteQuestion,
 } from "../controllers/questions";
+import { checkAuth } from "../config/utils";
 
 const QuestionRouter = express.Router();
 
-QuestionRouter.route("/").get(getAllQuestions).post(createQuestion);
+QuestionRouter.route("/").get(getAllQuestions).post(checkAuth, createQuestion);
 
 QuestionRouter.route("/:id")
   .get(getQuestionById)
-  .put(updateQuestion)
-  .delete(deleteQuestion);
+  .put(checkAuth, updateQuestion)
+  .delete(checkAuth, deleteQuestion);
 
 export { QuestionRouter };
